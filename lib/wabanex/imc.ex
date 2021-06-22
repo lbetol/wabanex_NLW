ca5ab6d56097e42d9466486078fdfac8e3634a49
Alberto Barrios <alberto.g.barrios@outlook.com>
2021-06-22 08:59:11 -0300

    wabanex

---
+++ lib/wabanex/imc.ex
@@ -0,0 +1,14 @@
+defmodule Wabanex.IMC do
+  def calculate(filename) do
+    filename
+    |> File.read()
+    |> handle_file()
+  end
+
+  defp handle_file({:ok, content}) do
+    content
+  end
+  defp handle_file({:error, reason}) do
+    reason
+  end
+end
